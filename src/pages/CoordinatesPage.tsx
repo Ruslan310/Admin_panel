3c00d260e175187b7f6b1c61d7ef620bed82c79d
akalantai <akalantai@fanpoint.dev>
2021-07-23 16:28:28 +0300

    auto assign modal to choose drivers added

--- src/pages/CoordinatesPage.tsx
+++ src/pages/CoordinatesPage.tsx
@@ -1,7 +1,21 @@
 import React, {useEffect, useState} from 'react'
 import {DataStore} from 'aws-amplify'
 
-import {Button, Form, Input, InputNumber, Layout, List, Select, Spin, Table} from 'antd';
+import {
+  Button,
+  Checkbox,
+  Col,
+  Form,
+  Input,
+  InputNumber,
+  Layout,
+  List,
+  Modal,
+  Select, Space,
+  Spin,
+  Table,
+  Typography
+} from 'antd';
 import {Address, Coordinate, Role, User} from "../models";
 import {ColumnsType} from "antd/es/table";
 import Title from "antd/es/typography/Title";
@@ -9,6 +23,7 @@ import {stringifyAddress} from "../utils/utils";
 import ManyPointsMapComponent from "../components/ManyPointsMapComponent";
 
 const {Content} = Layout;
+const {Text} = Typography;
 
 const width300 = {width: 300}
 
@@ -28,8 +43,10 @@ export interface Body {
 const CoordinatesPage: React.FC = () => {
   const [coordinates, setCoordinates] = useState<Coordinate[]>([]);
   const [drivers, setDrivers] = useState<User[]>([]);
+  const [selectedDrivers, setSelectedDrivers] = useState<User[]>([]);
   const [isLoading, setLoading] = useState(true);
   const [isDriversAssigning, setDriversAssigning] = useState(false);
+  const [isDriversModalVisible, setDriversModalVisible] = useState(false);
   const [name, setName] = useState('');
   const [latitude, setLatitude] = useState(0.000000);
   const [longitude, setLongitude] = useState(0.000000);
@@ -43,6 +60,7 @@ const CoordinatesPage: React.FC = () => {
   const fetchDrivers = async () => {
     const fetchedDrivers = await DataStore.query(User, user => user.role("eq", Role.DELIVERY));
     setDrivers(fetchedDrivers);
+    setSelectedDrivers(fetchedDrivers);
   }
 
   useEffect(() => {
@@ -138,148 +156,188 @@ const CoordinatesPage: React.FC = () => {
   }
 
   const driversAutoAssign = async () => {
+    if (selectedDrivers.length === 0) return;
     setDriversAssigning(true)
-    let body: Body = {addresses: [], clusters: drivers.length, drivers: drivers.map(driver => driver.id)};
-    for (const coordinate of coordinates) {
-      const geo: GeoAddress = {
-        drivers: [],
-        id: coordinate.id,
-        latitude: coordinate.latitude,
-        longitude: coordinate.longitude
+    if (selectedDrivers.length === 1) {
+      for (const coordinate of coordinates) {
+        await DataStore.save(
+          Coordinate.copyOf(coordinate, updated => {
+            updated.userID = selectedDrivers[0].id;
+          })
+        );
       }
-      body.addresses.push(geo)
-    }
-    const result = await fetch('https://gkjmmh4hi0.execute-api.us-east-1.amazonaws.com/getClusters', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(body)
-    })
-    const clusters = await result.json();
-    for (let key in clusters) {
-      if (clusters.hasOwnProperty(key)) {
-        let clusterCoordinates = clusters[key];
-        for (const clusterCoordinate of clusterCoordinates) {
-          const coordinate = await DataStore.query(Coordinate, clusterCoordinate)
-          if (coordinate) {
-            await DataStore.save(
-              Coordinate.copyOf(coordinate, updated => {
-                updated.userID = key;
-              })
-            );
+    } else {
+      let body: Body = {
+        addresses: [],
+        clusters: selectedDrivers.length,
+        drivers: selectedDrivers.map(driver => driver.id)
+      };
+      for (const coordinate of coordinates) {
+        const geo: GeoAddress = {
+          drivers: [],
+          id: coordinate.id,
+          latitude: coordinate.latitude,
+          longitude: coordinate.longitude
+        }
+        body.addresses.push(geo)
+      }
+      const result = await fetch('https://gkjmmh4hi0.execute-api.us-east-1.amazonaws.com/getClusters', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(body)
+      })
+      const clusters = await result.json();
+      for (let key in clusters) {
+        if (clusters.hasOwnProperty(key)) {
+          let clusterCoordinates = clusters[key];
+          for (const clusterCoordinate of clusterCoordinates) {
+            const coordinate = await DataStore.query(Coordinate, clusterCoordinate)
+            if (coordinate) {
+              await DataStore.save(
+                Coordinate.copyOf(coordinate, updated => {
+                  updated.userID = key;
+                })
+              );
+            }
           }
         }
       }
     }
     setDriversAssigning(false)
+    setDriversModalVisible(false);
   }
 
   return (
-    <Content>
-      <Title>Coordinates ({coordinates.length})</Title>
-      <Button
-        loading={isDriversAssigning}
-        disabled={isDriversAssigning}
-        onClick={driversAutoAssign} type="primary">
-        Auto assign drivers
-      </Button>
-      <Form
-        labelCol={{span: 4}}
-        wrapperCol={{span: 14}}
-        layout="horizontal"
-      >
-        <Form.Item label="Name" name="coordinatesName"
-                   rules={[{required: true, message: 'Please enter coordinates name!'}]}>
-          <Input style={width300} placeholder={'Enter name'} value={name} onChange={(e) => {
-            setName(e.target.value)
-          }}/>
-        </Form.Item>
-        <Form.Item label="Google map link">
-          <Input
-            style={width300}
-            onChange={(value) => {
-              const latReg = new RegExp("@(-?[\\d.]*)")
-              const latGroups = latReg.exec(value.target.value);
-              if (latGroups) {
-                setLatitude(parseFloat(latGroups[1]))
+    <>
+      <Modal
+        okButtonProps={{disabled: selectedDrivers.length === 0}}
+        title="Select drivers to assign"
+        visible={isDriversModalVisible}
+        onOk={driversAutoAssign}
+        onCancel={() => setDriversModalVisible(false)}>
+        <Space direction="vertical">
+          {selectedDrivers.length === 0 && <Text strong type="danger">And who do you think deliver it?</Text>}
+          {drivers.map((driver) =>
+            <Col span={8}>
+              <Checkbox
+                checked={selectedDrivers.includes(driver)}
+                onChange={(e) => {
+                  if (e.target.checked) {
+                    setSelectedDrivers([...selectedDrivers, driver])
+                  } else {
+                    setSelectedDrivers([...selectedDrivers.filter(sDriver => sDriver.id !== driver.id)])
+                  }
+                }}>{driver.email}</Checkbox>
+            </Col>)}
+        </Space>
+      </Modal>
+      <Content>
+        <Title>Coordinates ({coordinates.length})</Title>
+        <Button
+          loading={isDriversAssigning}
+          disabled={isDriversAssigning}
+          onClick={() => setDriversModalVisible(true)} type="primary">
+          Auto assign drivers
+        </Button>
+        <Form
+          labelCol={{span: 4}}
+          wrapperCol={{span: 14}}
+          layout="horizontal"
+        >
+          <Form.Item label="Name" name="coordinatesName"
+                     rules={[{required: true, message: 'Please enter coordinates name!'}]}>
+            <Input style={width300} placeholder={'Enter name'} value={name} onChange={(e) => {
+              setName(e.target.value)
+            }}/>
+          </Form.Item>
+          <Form.Item label="Google map link">
+            <Input
+              style={width300}
+              onChange={(value) => {
+                const latReg = new RegExp("@(-?[\\d.]*)")
+                const latGroups = latReg.exec(value.target.value);
+                if (latGroups) {
+                  setLatitude(parseFloat(latGroups[1]))
+                }
+                const lonReg = new RegExp("@[-?\\d.]*,([-?\\d.]*)")
+                const lonGroups = lonReg.exec(value.target.value);
+                if (lonGroups) {
+                  setLongitude(parseFloat(lonGroups[1]))
+                }
+              }}
+            />
+          </Form.Item>
+          <Form.Item label="latitude">
+            <InputNumber<string>
+              style={width300}
+              value={latitude.toString()}
+              min="-50"
+              max="50"
+              step="0.000001"
+              onChange={(value) => setLatitude(parseFloat(value))}
+              stringMode
+            />
+          </Form.Item>
+          <Form.Item label="longitude">
+            <InputNumber<string>
+              style={width300}
+              value={longitude.toString()}
+              min="-50"
+              max="50"
+              step="0.000001"
+              onChange={(value) => setLongitude(parseFloat(value))}
+              stringMode
+            />
+          </Form.Item>
+          <Form.Item wrapperCol={{offset: 4, span: 16}}>
+            <Button onClick={async () => {
+              if (name) {
+                await DataStore.save(
+                  new Coordinate({
+                    latitude: latitude,
+                    longitude: longitude,
+                    name: name,
+                  })
+                );
               }
-              const lonReg = new RegExp("@[-?\\d.]*,([-?\\d.]*)")
-              const lonGroups = lonReg.exec(value.target.value);
-              if (lonGroups) {
-                setLongitude(parseFloat(lonGroups[1]))
+            }} type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+        <Table
+          size={"middle"}
+          rowKey="id"
+          columns={columns}
+          dataSource={coordinates}
+          expandable={{
+            expandedRowRender: record => {
+              if (isLoadingAddresses) {
+                return <Spin size="large"/>
+              } else {
+                return <List
+                  size="small"
+                  dataSource={record.coordinateAddresses}
+                  renderItem={item => <List.Item>{stringifyAddress(item)}</List.Item>}
+                />
               }
-            }}
-          />
-        </Form.Item>
-        <Form.Item label="latitude">
-          <InputNumber<string>
-            style={width300}
-            value={latitude.toString()}
-            min="-50"
-            max="50"
-            step="0.000001"
-            onChange={(value) => setLatitude(parseFloat(value))}
-            stringMode
-          />
-        </Form.Item>
-        <Form.Item label="longitude">
-          <InputNumber<string>
-            style={width300}
-            value={longitude.toString()}
-            min="-50"
-            max="50"
-            step="0.000001"
-            onChange={(value) => setLongitude(parseFloat(value))}
-            stringMode
-          />
-        </Form.Item>
-        <Form.Item wrapperCol={{offset: 4, span: 16}}>
-          <Button onClick={async () => {
-            if (name) {
-              await DataStore.save(
-                new Coordinate({
-                  latitude: latitude,
-                  longitude: longitude,
-                  name: name,
-                })
-              );
+            },
+            rowExpandable: record => true,
+            onExpand: async (expanded, record) => {
+              if (expanded) {
+                await loadAddresses(record.id)
+              }
             }
-          }} type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-      <Table
-        size={"middle"}
-        rowKey="id"
-        columns={columns}
-        dataSource={coordinates}
-        expandable={{
-          expandedRowRender: record => {
-            if (isLoadingAddresses) {
-              return <Spin size="large"/>
-            } else {
-              return <List
-                size="small"
-                dataSource={record.coordinateAddresses}
-                renderItem={item => <List.Item>{stringifyAddress(item)}</List.Item>}
-              />
-            }
-          },
-          rowExpandable: record => true,
-          onExpand: async (expanded, record) => {
-            if (expanded) {
-              await loadAddresses(record.id)
-            }
-          }
-        }}
-      />
-      {!isLoading && coordinates.length > 0 && <ManyPointsMapComponent
-        // @ts-ignore
-        center={{lat: 34.6671732, lng: 33.0132906}}
-        zoom={12}
-        places={coordinates}
-      />}
-    </Content>
+          }}
+        />
+        {!isLoading && coordinates.length > 0 && <ManyPointsMapComponent
+          // @ts-ignore
+          center={{lat: 34.6671732, lng: 33.0132906}}
+          zoom={12}
+          places={coordinates}
+        />}
+      </Content>
+    </>
   )
 }
 

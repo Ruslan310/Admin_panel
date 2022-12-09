386297180001a1663f3cfa4a0bcd5185dfcffde6
shmublon <eriloN@3start>
2022-12-09 12:35:02 +0200

    refactoring customer page done

--- src/pages/CustomersPage.tsx
+++ src/pages/CustomersPage.tsx
@@ -2,76 +2,83 @@ import React, {useEffect, useState} from 'react'
 
 import {Layout, Table, Typography} from 'antd';
 import {ColumnsType} from "antd/es/table";
-import {Customer} from "../API";
-import {fetchCustomers} from "../graphql/requests";
+import {Coordinate, Customer} from "../models";
+import {DataStore} from "aws-amplify";
 
 const {Content} = Layout;
 const {Title} = Typography;
 
 const CustomersPage: React.FC = () => {
-  const [customers, setCustomers] = useState<Customer[]>([]);
+    const [customers, setCustomers] = useState<Customer[]>([]);
+    const [isLoading, setLoading] = useState(true);
 
-  const loadCustomers = async () => {
-    const fetchedCustomers = await fetchCustomers();
-    setCustomers(fetchedCustomers);
-  }
+    useEffect(() => {
+        DataStore.observeQuery(Customer)
+            .subscribe(msg => {
+                if (msg.isSynced) {
+                    setCustomers(msg.items)
+                    isLoading && setLoading(false)
+                } else {
+                    if (msg.items.length > 0) {
+                        setCustomers(msg.items)
+                        isLoading && setLoading(false)
+                    }
+                }
+            });
+    }, []);
 
-  useEffect(() => {
-    loadCustomers();
-  }, []);
+    const columns: ColumnsType<Customer> = [
+        {
+            title: 'First name',
+            dataIndex: 'firstName',
+        },
+        {
+            title: 'Last name',
+            dataIndex: 'lastName',
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+        },
+        {
+            title: 'Company',
+            dataIndex: 'company',
+            sorter: (a, b) => {
+                if (!a.company) {
+                    return -1;
+                }
+                if (a.company && b.company) {
+                    if (a.company < b.company) {
+                        return -1;
+                    }
+                    if (a.company > b.company) {
+                        return 1;
+                    }
+                }
+                return 0;
+            },
+            defaultSortOrder: "ascend"
+        },
+    ];
 
-  const columns: ColumnsType<Customer> = [
-    {
-      title: 'First name',
-      dataIndex: 'firstName',
-    },
-    {
-      title: 'Last name',
-      dataIndex: 'lastName',
-    },
-    {
-      title: 'Email',
-      dataIndex: 'email',
-    },
-    {
-      title: 'Company',
-      dataIndex: 'company',
-      sorter: (a, b) => {
-        if (!a.company) {
-          return -1;
-        }
-        if (a.company && b.company) {
-          if (a.company < b.company) {
-            return -1;
-          }
-          if (a.company > b.company) {
-            return 1;
-          }
-        }
-        return 0;
-      },
-      defaultSortOrder: "ascend"
-    },
-  ];
-
-  return (
-    <Content>
-      <Title>Customers ({customers.length})</Title>
-      {/*<Button onClick={async () => {*/}
-      {/*  for (const customer of customers) {*/}
-      {/*    await DataStore.delete(Customer, customer.id);*/}
-      {/*  }*/}
-      {/*}} type="primary" htmlType="submit">*/}
-      {/*  Delete all customers*/}
-      {/*</Button>*/}
-      <Table
-        size={"middle"}
-        rowKey="id"
-        columns={columns}
-        dataSource={customers}
-      />
-    </Content>
-  )
+    return (
+        <Content>
+            <Title>Customers ({customers.length})</Title>
+            {/*<Button onClick={async () => {*/}
+            {/*  for (const customer of customers) {*/}
+            {/*    await DataStore.delete(Customer, customer.id);*/}
+            {/*  }*/}
+            {/*}} type="primary" htmlType="submit">*/}
+            {/*  Delete all customers*/}
+            {/*</Button>*/}
+            <Table
+                size={"middle"}
+                rowKey="id"
+                columns={columns}
+                dataSource={customers}
+            />
+        </Content>
+    )
 }
 
 export default CustomersPage
